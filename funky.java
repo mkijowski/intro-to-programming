1cbaa783294af6008d78c9d3d81cfb1bf134d329
mkijowski <matthewkijowski@gmail.com>
2017-05-23 16:46:28 -0400

    java might helloworld

--- funky.java
+++ funky.java
@@ -1,1 +1,16 @@
-IDK, some package bullshit i guess
+//IDK, some package bullshit i guess
+
+
+
+public class MyFunkyPrint(String s) {
+	System.out.println(s);
+    }
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+	System.out.println("Hello, World");
+    }
+    
+}
