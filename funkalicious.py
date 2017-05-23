4a8e2d98482c2f5c8f651e75eff4c9b6ce6dff3e
mkijowski <matthewkijowski@gmail.com>
2017-05-23 17:19:20 -0400

    did it in python as well ish

---
+++ funkalicious.py
@@ -0,0 +1,9 @@
+import sys
+
+def printme( str ):
+    "This prints a passed string into this function"
+    print str
+    return
+
+printme("Give me a string terminating in a newline")
+printme(raw_input())
