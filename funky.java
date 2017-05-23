6f6a90d6c96190e1de99c141147e52da96e88dc6
mkijowski <matthewkijowski@gmail.com>
2017-05-23 17:12:30 -0400

    java working?  go kayligh!!!

--- funky.java
+++ funky.java
@@ -2,13 +2,18 @@
 
 
 
-//public class MyFunkyPrint(String s) {
-//	System.out.println(s);
-//   }
+//private class MyFunkyPrint {
+//	string s;    
+//}
 
 public class funky {
+    static String mystring(String words){
+	return words;
+    }
 
     public static void main(String[] args) {
+	String crazyString = "Don't go crazy";
+	System.out.println(mystring(crazyString));
     // Prints "Hello, World" to the terminal window.
 	System.out.println("Hello, World");
     }
